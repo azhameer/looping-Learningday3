c617109ffba487ac836d19ce989492339fbe3d5e
Zameer <azhameer13@gmail.com>
2026-09-20 16:17:24 +0700

    Implement loop to sum numbers up to user input

---
+++ loopPlusNumber.py
@@ -0,0 +1,9 @@
+number = int(input("Bilangan yang ingin dimasukkan: "))
+
+total = 0
+i = 1
+while i <= number:
+    total += i
+    i += 1  
+
+print("Hasil penjumlahan dari 1 sampai", number, "adalah", total)
